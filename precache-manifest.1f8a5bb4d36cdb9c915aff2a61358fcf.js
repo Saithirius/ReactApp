28d8a49b3f93ac32c71744a62dfb57d3dc5f9e9c
Saithirius <look1899@mail.ru>
2020-06-08 22:13:23 +0500

    Updates

---
+++ precache-manifest.1f8a5bb4d36cdb9c915aff2a61358fcf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2abccf76322c8f74f21c7fe05d00da0",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "8029a277b7beafb03872",
+    "url": "/ReactApp/static/css/main.1e5fa60c.chunk.css"
+  },
+  {
+    "revision": "75b87d9c3d824608bc8f",
+    "url": "/ReactApp/static/js/2.dfc05adf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactApp/static/js/2.dfc05adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8029a277b7beafb03872",
+    "url": "/ReactApp/static/js/main.ba890a22.chunk.js"
+  },
+  {
+    "revision": "3d0fb9fc68266813a5ba",
+    "url": "/ReactApp/static/js/runtime-main.b525e3b2.js"
+  },
+  {
+    "revision": "f66ac0433111a716b8831a5a351df086",
+    "url": "/ReactApp/static/media/defPhotoLarge.f66ac043.jpg"
+  },
+  {
+    "revision": "8a8b9b5d1cee19d663232df40e4389e4",
+    "url": "/ReactApp/static/media/defPhotoSmall.8a8b9b5d.jpg"
+  },
+  {
+    "revision": "bd0264b815e4e8911c38590617bf14e9",
+    "url": "/ReactApp/static/media/logo.bd0264b8.svg"
+  },
+  {
+    "revision": "3add48589b930776dcf51eab0f10fc0a",
+    "url": "/ReactApp/static/media/preloader.3add4858.svg"
+  }
+]);
