929ff8d3aad3ae21c13708e6bd2c9c808a2f4640
Saithirius <look1899@mail.ru>
2020-06-08 22:06:34 +0500

    add git-page

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 window.state = store.getState;
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
